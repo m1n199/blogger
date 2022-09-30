ed71ef319295a5d8003443e5e9f6a3591aa1027b
Metsu <13114199adarsh@gmail.com>
2022-09-30 06:38:20 +0000

    done

--- temp/script.js
+++ temp/script.js
@@ -1,30 +1,31 @@
 !function () {
 
         $('#header-toggle').on("click", () => {
-            //to-know sidebar is active ?
-            $('body').toggleClass('is-sidebar');
-            // // close .sidebar-content div if it was open
-            // if (!$('.sidebar-content').hasClass('close-sidebar-content')) $('.sidebar-content').addClass('close-sidebar-content');
-            // // show navbar when bar clicked and make bg dark
-            // if ($('#nav-bar').toggleClass('show').hasClass('show')) $('#content').removeClass('bg-light');
-            // else $('#content').addClass('bg-light');
-            // // change bars icon to cross
-            // $('#header-toggle').toggleClass('bx-x');
-            // // add padding to header
-            // $('#header').toggleClass('body-pd');
+            //to set operation for sidebar when it's open through CSS...
+            //to close sidebar-content when side bar is closed 
+            if (!$('body').toggleClass('is-sidebar').hasClass('is-sidebar') && $('body').hasClass('is-sidebarIcon')) 
+                $('body').removeClass('is-sidebarIcon');
         });
     
     /*===== LINK ACTIVE =====*/
-    $('.nav_link').each( function(){
-        $(this).on("click",() => {
-            $('.nav_link.active').removeClass('active');
-            $(this).addClass('active');
-            if ($('.sidebar-content').hasClass('close-sidebar-content')) $('.sidebar-content').removeClass('close-sidebar-content');
-            if ($('#content').hasClass('bg-light')) $('#content').removeClass('bg-light');
-            // $('.sidebar-content .widget')[$(this).index()].addClass('display');
+    $('.nav_link').click(function(){
+            $('.sidebar-content .widget')
+                .removeClass('display');
 
-
-        })
-        
+            $('.sidebar-content .widget')
+                .eq($(this).index())
+                .addClass('display');
+            
+            if($(this).hasClass('active')) {
+                $('body')
+                    .removeClass('is-sidebarIcon');
+                $(this).removeClass('active');
+            }
+            else {
+                $('body')
+                    .addClass('is-sidebarIcon');
+                $('.nav_link').removeClass('active');
+                $(this).addClass('active');
+            }
     });
 }(jQuery);
